bf5d557d8144d95443a8b6d3371d2d24d6083fc5
Bài 1+2 <huytit98@gmail.com>
2018-09-25 21:21:24 +0700

    BT buoi1

--- handout/Practice1/searchPractice.js
+++ handout/Practice1/searchPractice.js
@@ -1,7 +1,18 @@
 'use strict'
 
 function search(input, target) {
-  return  input.indexOf(target);  // Remove this line and change to your own algorithm
+  // return  input.indexOf(target);  // Remove this line and change to your own algorithm
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] >= 10000 || input[i] <= -10000) {
+      break;
+    }
+    if (input[i] >= input[i + 1]) {
+      break;
+    }
+    if (input[i] == target)
+      return i + 1;
+  }
+  return -1;
 }
 
 module.exports = search
--- handout/Practice2/sortPractice.js
+++ handout/Practice2/sortPractice.js
@@ -1,7 +1,16 @@
 'use strict'
 
 function sort(input) {
-  return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  // return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+  for(let i = 0; i < input.length - 1; i++){
+    for(let j = input.length - 1; j > i; j--){
+      if(input[j] < input[j-1]){
+        let t = input[j];
+        input[j] = input[j - 1];
+        input[j - 1] = t; 
+      }
+    }
+  }
 }
 
 module.exports = sort
